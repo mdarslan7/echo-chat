7dc1021f83b7b498041567c04e8840c663522200
mdarslan7 <mdarslan7@gmail.com>
2025-02-14 01:10:37 +0530

    clean up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ export default function SignUp() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-100 text-white">
       <div className="w-full max-w-md p-8 space-y-6 border border-gray-300 rounded-lg bg-white text-black">
-        <h2 className="text-2xl font-semibold text-center">Sign Up</h2>
+        <h2 className="text-2xl font-bold text-center">Sign Up</h2>
         <form className="space-y-4">
           <div>
             <label className="block text-sm font-medium" htmlFor="email">
